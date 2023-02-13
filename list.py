126105c9b91b7be36027b4feff1685e2dae41048
2005muskan <muskankushwah131@gmail.com>
2023-02-13 23:57:44 +0530

    simple commit

---
+++ list.py
@@ -0,0 +1,18 @@
+list1 = [1,2,3,"kushwah",4,5]
+print(list1)
+
+print("+++++++++++++++++++++++")
+
+list1.append("Hello")
+print(list1)
+
+print("++++++++++++++++++++++++")
+
+list1.reverse()
+print(list1)
+
+print("++++++++++++++++++++++++")
+list1.insert(2,"10")
+print(list1)
+print("++++++++++++++++++++++++")
+print("thankyou")
